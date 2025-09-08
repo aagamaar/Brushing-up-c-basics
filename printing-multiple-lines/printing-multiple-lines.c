43ffc37ce56a2c770ab0ae6c0be8be247389cbdd
aagamaar <aagamaar01@gmail.com>
2025-09-08 18:54:57 +0530

    Added 3 diaplay lines

--- printing-multiple-lines/printing-multiple-lines.c
+++ printing-multiple-lines/printing-multiple-lines.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    
+
     /*Added
     a
-    multiline 
+    multiline
     comment
     */
 
-    printf("I like you very much.\nI love Kochi !");
+    printf("I like you very much.\nI love Kochi !\n");
 
-    //Compiler ignores this line
+    // Compiler ignores this line
 
     int a = 10;
     float b = 2.4;
-    char c = "t";
+    char c = 't';
+
+    printf("int a = %d\n", a);
+    printf("int b = %f\n", b);
+    printf("int c = %c\n", c);
     
     return 0;
 }
