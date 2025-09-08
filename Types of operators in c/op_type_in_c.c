b59a2a2d96deb66171d4928bb79b616883780337
aagamaar <aagamaar01@gmail.com>
2025-09-08 19:50:50 +0530

    edited the code

--- Types of operators in c/op_type_in_c.c
+++ Types of operators in c/op_type_in_c.c
@@ -2,7 +2,15 @@
 int main()
 {
     // Arithmetic Operators
-    int a = 6, b = 7, c = 8;
-    printf("The sum of %d and %d = %d", a, b, a + b);
+    int a = 10, b = 8, c = 9;
+    printf("The sum of %d and %d = %d\n", a, b, a + b);
+    printf("The difference of %d and %d = %d\n", a, b, a - b);
+    printf("The product of %d and %d = %d\n", a, b, a * b);
+    printf("The quotient of %d and %d = %d\n", a, b, a/b);
+    printf("The modulo division of %d and %d = %d\n", a, b, a%b);
+    printf("The increment result of a = %d\n",++a);
+    printf("The decrement result of a = %d",--a);
+    
+
     return 0;
 }
