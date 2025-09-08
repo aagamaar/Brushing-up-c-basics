e236ea9f354bd63d0a78a6c3a99c785af18255ff
aagamaar <aagamaar01@gmail.com>
2025-09-08 18:49:37 +0530

    Added a multiline comment

--- printing-multiple-lines/printing-multiple-lines.c
+++ printing-multiple-lines/printing-multiple-lines.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main()
 {
+    
+    /*Added
+    a
+    multiline 
+    comment
+    */
     printf("I like you very much.\nI love Kochi !");
     //Compiler ignores this line
     return 0;
